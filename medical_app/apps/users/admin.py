c844b44e8468a950599b23991cc67bd124c496af
annapohoreltseva <annapogorelceva8@gmail.com>
2026-07-30 22:41:16 +0300

    Connected exhisting models to Django Admin using class creating sythasics

--- medical_app/apps/users/admin.py
+++ medical_app/apps/users/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from apps.users.models import User
 
 # Register your models here.
-admin.site.register(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
